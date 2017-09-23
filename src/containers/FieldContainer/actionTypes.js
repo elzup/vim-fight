f6f1eaaa16f3d0a2a35f8950be775efc515e6d34
elzup <guild0105@gmail.com>
2017-09-24 04:39:16 +0900

    Add ReceiveKey Type

--- src/containers/FieldContainer/actionTypes.js
+++ src/containers/FieldContainer/actionTypes.js
@@ -3,9 +3,12 @@ import type { Field } from "../../types";
 
 export const RECEIVE_FIELD: "FieldContainer/RECEIVE_FIELD" =
   "FieldContainer/RECEIVE_FIELD";
+export const RECEIVE_KEY: "FieldContainer/RECEIVE_KEY" =
+  "FieldContainer/RECEIVE_KEY";
 
 export const Actions = {
-  RECEIVE_FIELD
+  RECEIVE_FIELD,
+  RECEIVE_KEY
 };
 
 export type ReceiveField = {
@@ -13,4 +16,9 @@ export type ReceiveField = {
   field: Field
 };
 
-export type Action = ReceiveField;
+export type ReceiveKey = {
+  type: typeof RECEIVE_KEY,
+  key: string
+};
+
+export type Action = ReceiveField | ReceiveKey;
--- src/types/state.js
+++ src/types/state.js
@@ -1,12 +1,14 @@
 // @flow
 import type { State as CartContainer } from "../containers/CartContainer/reducer";
 import type { State as FieldContainer } from "../containers/FieldContainer/reducer";
+import type { State as Key } from "../containers/Key/reducer";
 import type { State as ProductById } from "../containers/ProductById/reducer";
 import type { State as ProductsContainer } from "../containers/ProductsContainer/reducer";
 
 export type State = {
   CartContainer: CartContainer,
   FieldContainer: FieldContainer,
+  Key: Key,
   ProductById: ProductById,
   ProductsContainer: ProductsContainer
 };
